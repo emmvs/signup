fd4debf2a65661a41127147d0b2cc62d2ab6f31c
Emma Rünzel <emma@ruenzel.de>
2022-02-17 21:25:56 +0100

    cloudy minds

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,2 +1,6 @@
 class WordsController < ApplicationController
+
+def article_params
+  params.require(:word).permit(:title, :description, :video)
+end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -3,4 +3,5 @@ class Word < ApplicationRecord
   belongs_to :category
   has_many :bookmarks, dependent: :destroy
   validates :title, :language, presence: true
+  has_one_attached :video
 end
