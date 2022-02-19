e77049b274c8b13ec99fc11f7053037d0b1d2d72
Emma Rünzel <emma@ruenzel.de>
2022-02-19 13:01:48 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :signs do
+  resources :signs
+  # resources :comments, only: [ :new, :create ] - should be nested inside :signs
+  # resources :comments, only: [ :edit, :update, :destroy ]
+  resources :bookmarks, only: [ :index, :destroy ]
+  resources :categories, only: [ :index, :show ]
+  resources :lists do
     resources :bookmarks, only: [ :new, :create ]
-    resources :comments, only: [ :new, :create, :update, :destroy ]
   end
-  resources :bookmarks, only: [ :update, :destroy ]
-  resources :categories, only: [ :index ]
+  # get 'dashboard', to: 'pages#dashboard'
 end
