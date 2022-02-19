4d19cd929cd13ca455edece25afc5ebd471410bb
Dustin Dillenburg <dustin.dillenburg@gmail.com>
2022-02-19 17:27:33 +0100

    video integrated

--- app/models/sign.rb
+++ app/models/sign.rb
@@ -2,6 +2,6 @@ class Sign < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :bookmarks, dependent: :destroy
-  validates :title, :language, presence: true
-  has_one_attached :sign
+  validates :title, presence: true
+  has_one_attached :video
 end
