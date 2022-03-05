c34a69b4bcd988acdb189cc2e3ad5dc0e3c50d03
Emma Rünzel <emma@ruenzel.de>
2022-03-05 12:22:21 +0100

    Halleluja the video upload fuction is finally working

--- app/javascript/components/record_sign.js
+++ app/javascript/components/record_sign.js
@@ -1,59 +1,82 @@
 // // signup/app/javascript/components/record_sign.js
 
-import Rails from '@rails/ujs';
-const initRecordVideo = () => {
-  const start = document.getElementById('start');
-  const stop = document.getElementById('stop');
-  const live = document.getElementById('live');
+import Rails from "@rails/ujs";
+
+const recordSign = () => {
+  // console.log(start)
+  const start = document.getElementById("start");
+  if(start) {
+  const stop = document.getElementById("stop");
+  const live = document.getElementById("live");
   const form = document.querySelector('.video_form');
-  const uploadToCloudinary = (video) => {
-    const formData = new FormData(form);
-    formData.append('sign[video]', video, 'my_video.mp4');
-    Rails.ajax({
-      url: '/signs',
-      type: 'post',
-      data: formData,
-    });
-  };
   const stopVideo = () => {
-    live.srcObject.getTracks().forEach((track) => track.stop());
-  };
+    live.srcObject.getTracks().forEach(track => track.stop());
+  }
+
   // stop.addEventListener("click", stopVideo);
+
   const stopRecording = () => {
-    return new Promise((resolve) => stop.addEventListener('click', resolve));
-  };
+    return new Promise(resolve => stop.addEventListener("click", resolve));
+  }
+
   const startRecording = (stream) => {
     const recorder = new MediaRecorder(stream);
     let data = [];
-    recorder.ondataavailable = (event) => data.push(event.data);
+
+    recorder.ondataavailable = event => data.push(event.data);
     recorder.start();
+
     const stopped = new Promise((resolve, reject) => {
       recorder.onstop = resolve;
-      recorder.onerror = (event) => reject(event.name);
+      recorder.onerror = event => reject(event.name);
     });
-    const recorded = stopRecording().then(() => {
-      stopVideo();
-      recorder.state == 'recording' && recorder.stop();
-    });
-    return Promise.all([stopped, recorded]).then(() => data);
-  };
-  start.addEventListener('click', () => {
-    navigator.mediaDevices
-      .getUserMedia({
-        video: true,
-        audio: true,
-      })
-      .then((stream) => {
-        live.srcObject = stream;
-        live.captureStream = live.captureStream || live.mozCaptureStream;
-        return new Promise((resolve) => (live.onplaying = resolve));
-      })
-      .then(() => startRecording(live.captureStream()))
-      .then((recordedChunks) => {
-        const recordedBlob = new Blob(recordedChunks, { type: 'video/mp4' });
-        console.log(recordedBlob);
-        uploadToCloudinary(recordedBlob);
-      });
+
+    const recorded = stopRecording().then(
+      () => {
+        stopVideo();
+        recorder.state == "recording" && recorder.stop();
+      }
+    );
+
+    return Promise.all([
+      stopped,
+      recorded
+    ])
+    .then(() => data);
+  }
+
+  const uploadToCloudinary  = (video) => {
+    const formData = new FormData(document.querySelector('.video_form'));
+    let date = new Date().getTime
+    formData.append('sign[video]', video, `${date}_my_video.mp4`);
+    console.log(formData, video)
+
+    Rails.ajax({
+      url: "/signs",
+      type: "post",
+      data: formData,
+    })
+  }
+
+  start.addEventListener("click", (e) => {
+    e.preventDefault();
+    navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true
+    })
+    .then(stream => {
+      live.srcObject = stream;
+      live.captureStream = live.captureStream || live.mozCaptureStream;
+      return new Promise(resolve => live.onplaying = resolve);
+    })
+    .then(() => startRecording(live.captureStream()))
+    .then (recordedChunks => {
+
+      const recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
+      uploadToCloudinary(recordedBlob);
+    })
   });
-};
-export { initRecordVideo };
+ }
+}
+
+export { recordSign };
