5a0eb7a2dcd4c122ce90cd2bed430c47a493f2c3
Emma Rünzel <emma@ruenzel.de>
2022-03-17 16:25:44 +0100

    added Swedish to the mess

--- app/models/sign.rb
+++ app/models/sign.rb
@@ -5,7 +5,7 @@ class Sign < ApplicationRecord
   validates :title, presence: true
   has_one_attached :video
   enum status: { open: 0, verified: 1 }
-  enum language: { german: 0, english: 1, french: 2 , albanian: 3, bulgarian: 4 }
+  enum language: { german: 0, english: 1, french: 2 , swedish: 3, albanian:4, bulgarian: 5 }
 
   include PgSearch::Model
   multisearchable against: [:title, :description, :language]
